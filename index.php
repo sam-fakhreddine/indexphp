ec3e700a2f5c70acced5d8dcc740b371e9c27022
sam-fakhreddine <sam-fakhreddine@users.noreply.github.com>
2019-05-05 22:24:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<html>
-<head>
-  <title>Sam Fakhreddine: Next Steps / AWS Test - DevOps Engineer - Onica</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<?phpprint <html>?>
+<?php<head>?>
+  <?php<title>Sam Fakhreddine: Next Steps / AWS Test - DevOps Engineer - Onica</title>?>
+  <?php<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">?>
 </head>
 <body>
   <h1>Welcome to the Sam's Demo Webpage!</h1>
@@ -22,24 +22,25 @@
       $hostname = curl_exec($curl_handle);
       if (empty($hostname))
       {
-       print "Sorry, for some reason, we got no hostname back <br />";
+       print "Sorry, for some reason, we got no hostname back. <br>" ;
       }
       else
       {
-       print "Server = " . $hostname . "<br />";
+       print "Server = " . $hostname . ".<br>";
       }
       // Get the instance-id of the intance from the instance metadata
       curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/instance-id');
       $instanceid = curl_exec($curl_handle);
       if (empty($instanceid))
       {
-       print "Sorry, for some reason, we got no instance id back <br />";
+       print "Sorry, for some reason, we got no instance id back.<br>";
       }
       else
       {
-       print "EC2 instance-id = " . $instanceid . "<br />";
+       print "EC2 instance-id = " . $instanceid . ".<br>";
       }
+  ?>
     <h2>Thanks for coming by!</h2>
-    <p/>
+	
   </body>
 </html>
